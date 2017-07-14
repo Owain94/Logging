e7a59e3db1cfcf7d0655547f09c645bc2672db81
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-07-14 04:00:19 +0200

    Unit test the store reducers

--- src/app/store/reducers/case.reducer.spec.ts
+++ src/app/store/reducers/case.reducer.spec.ts
@@ -1,6 +1,38 @@
-import { caseReducer } from './case.reducer';
+import { caseReducer, initialState } from './case.reducer';
+import { CaseActions } from '../actions/case.actions';
+
+const state = {
+  data: [
+    {
+      _id: '1',
+      name: 'test',
+      description: 'test',
+    }
+  ],
+  type: 'LOAD_CASES',
+  error: false
+};
+
+const payload = {
+  _id: '',
+  name: '',
+  description: ''
+}
+
+const payloadEdit = {
+  _id: '1',
+  name: '',
+  description: ''
+}
 
 describe('The case reducer', () => {
+  it('initial state should return empty', () => {
+    expect(
+      initialState()
+    )
+    .toEqual([]);
+  });
+
   it('should handle initial state', () => {
     expect(
       caseReducer([], { type: '' })
@@ -13,6 +45,53 @@ describe('The case reducer', () => {
       caseReducer([], { type: '' })
     )
     .toEqual([]);
+  });
+
+  it('should handle LOAD_CASES_SUCCESS', () => {
+    const subject = caseReducer([], { type: CaseActions.LOAD_CASES_SUCCESS, payload: state })
+    expect(subject.error).toBeFalsy();
+  });
+
+  it('should handle ADD_CASE_SUCCESS', () => {
+    const subject = caseReducer(state, { type: CaseActions.ADD_CASE_SUCCESS, payload: payload })
+    expect(subject.data.length).toEqual(2);
+    expect(subject.error).toBeFalsy();
+  });
+
+  it('should handle ADD_CASE_FAILURE', () => {
+    const subject = caseReducer(state, { type: CaseActions.ADD_CASE_FAILURE, payload: payload })
+    expect(subject.data.length).toEqual(1);
+    expect(subject.error).toBeTruthy();
+  });
+
+  it('should handle EDIT_CASE_SUCCESS', () => {
+    const subject = caseReducer(state, { type: CaseActions.EDIT_CASE_SUCCESS, payload: payloadEdit })
+    expect(subject.data.length).toEqual(1);
+    expect(subject.data[0].name).toEqual('');
+    expect(subject.data[0].description).toEqual('');
+    expect(subject.error).toBeFalsy();
+  });
+
+  it('should handle EDIT_CASE_FAILURE', () => {
+    const subject = caseReducer(state, { type: CaseActions.EDIT_CASE_FAILURE, payload: payloadEdit })
+    expect(subject.data.length).toEqual(1);
+    expect(subject.data[0].name).toEqual('test');
+    expect(subject.data[0].description).toEqual('test');
+    expect(subject.error).toBeTruthy();
+  });
+
+  it('should handle DELETE_CASE_SUCCESS', () => {
+    const subject = caseReducer(state, { type: CaseActions.DELETE_CASE_SUCCESS, payload: payloadEdit })
+    expect(subject.data.length).toEqual(0);
+    expect(subject.error).toBeFalsy();
+  });
+
+  it('should handle DELETE_CASE_FAILURE', () => {
+    const subject = caseReducer(state, { type: CaseActions.DELETE_CASE_FAILURE, payload: payloadEdit })
+    expect(subject.data.length).toEqual(1);
+    expect(subject.data[0].name).toEqual('test');
+    expect(subject.data[0].description).toEqual('test');
+    expect(subject.error).toBeTruthy();
   });
 
 });
--- src/app/store/reducers/settings.reducer.spec.ts
+++ src/app/store/reducers/settings.reducer.spec.ts
@@ -1,6 +1,44 @@
-import { settingsReducer } from './settings.reducer';
+import { settingsReducer, initialState } from './settings.reducer';
+import { SettingsActions } from '../actions/settings.actions';
+
+const state = {
+  data: [
+    {
+      _id: '1',
+      name: 'test',
+      'case': '1',
+      invpre: 'test',
+      location: 'test',
+    }
+  ],
+  type: 'LOAD_SETTINGS',
+  error: false
+}
+
+const payload = {
+  _id: '',
+  name: '',
+  'case': '',
+  invpre: '',
+  location: '',
+}
+
+const payloadEdit = {
+  _id: '1',
+  name: '',
+  'case': '',
+  invpre: '',
+  location: '',
+}
 
 describe('The settings reducer', () => {
+  it('initial state should roturn empty', () => {
+    expect(
+      initialState()
+    )
+    .toEqual([]);
+  });
+
   it('should handle initial state', () => {
     expect(
       settingsReducer([], { type: '' })
@@ -13,6 +51,43 @@ describe('The settings reducer', () => {
       settingsReducer([], { type: '' })
     )
     .toEqual([]);
+  });
+
+  it('should handle LOAD_SETTINGS_SUCCESS', () => {
+    const subject = settingsReducer([], { type: SettingsActions.LOAD_SETTINGS_SUCCESS, payload: state })
+    expect(subject.error).toBeFalsy();
+  });
+
+  it('should handle ADD_SETTINGS_SUCCESS', () => {
+    const subject = settingsReducer(state, { type: SettingsActions.ADD_SETTINGS_SUCCESS, payload: payload })
+    expect(subject.data.length).toEqual(2);
+    expect(subject.error).toBeFalsy();
+  });
+
+  it('should handle ADD_SETTINGS_FAILURE', () => {
+    const subject = settingsReducer(state, { type: SettingsActions.ADD_SETTINGS_FAILURE, payload: payload })
+    expect(subject.data.length).toEqual(1);
+    expect(subject.error).toBeTruthy();
+  });
+
+  it('should handle EDIT_SETTINGS_SUCCESS', () => {
+    const subject = settingsReducer(state, { type: SettingsActions.EDIT_SETTINGS_SUCCESS, payload: payloadEdit })
+    expect(subject.data.length).toEqual(1);
+    expect(subject.data[0].name).toEqual('');
+    expect(subject.data[0].case).toEqual('');
+    expect(subject.data[0].invpre).toEqual('');
+    expect(subject.data[0].location).toEqual('');
+    expect(subject.error).toBeFalsy();
+  });
+
+  it('should handle EDIT_SETTINGS_FAILURE', () => {
+    const subject = settingsReducer(state, { type: SettingsActions.EDIT_SETTINGS_FAILURE, payload: payloadEdit })
+    expect(subject.data.length).toEqual(1);
+    expect(subject.data[0].name).toEqual('test');
+    expect(subject.data[0].case).toEqual('1');
+    expect(subject.data[0].invpre).toEqual('test');
+    expect(subject.data[0].location).toEqual('test');
+    expect(subject.error).toBeTruthy();
   });
 
 });
