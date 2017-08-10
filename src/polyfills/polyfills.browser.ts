4248596c2eb08ba8a4197757dbc8df1ff1bb1276
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-08-10 02:12:24 +0200

    Smoothscroll wrapper

--- src/polyfills/polyfills.browser.ts
+++ src/polyfills/polyfills.browser.ts
@@ -2,5 +2,4 @@ import './polyfills';
 import 'zone.js/dist/zone';
 import 'window.requestanimationframe';
 import 'web-animations-js';
-
-require('smoothscroll-polyfill').polyfill();
+import 'scroll-behaviour/polyfill'
