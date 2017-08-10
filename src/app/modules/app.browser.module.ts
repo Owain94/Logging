154a0c066b58d39878077ae403e9a11574c55aa0
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-08-10 20:25:07 +0200

    Remove BrowserAnimationsModule

--- src/app/modules/app.browser.module.ts
+++ src/app/modules/app.browser.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { WorkerAppModule, WORKER_APP_LOCATION_PROVIDERS } from '@angular/platform-webworker';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppModule } from './app.module';
 
@@ -8,7 +7,6 @@ import { MainComponent } from '../components/main/main.component';
 
 @NgModule({
   imports: [
-    BrowserAnimationsModule,
     WorkerAppModule,
     AppModule
   ],
